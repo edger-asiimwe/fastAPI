e6498b541c49b62dba55df80f745293091d722fe
edger-asiimwe <edgerasiimwe@gmail.com>
2022-03-18 05:59:48 +0300

    Responze body and status code

--- api/main.py
+++ api/main.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from os import stat
 from typing import Optional
 from xmlrpc.client import boolean
-from fastapi import Body, Cookie, FastAPI, Query, Path, Header
+from fastapi import Body, Cookie, FastAPI, Query, Path, Header, status
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -21,18 +22,25 @@ class ModelName(str, Enum):
     third = "third"
 
 
-@app.get("/")
+class Payment(BaseModel):
+    amount: float
+    currency: str
+    first_name: str
+    last_name: str
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
 def root():
     return {"message": "Welcome to my API"}
 
 
 
-@app.get("/post")
+@app.get("/post", status_code=status.HTTP_200_OK)
 def get_post():
     return {"data": "This is a post"}
 
 
-@app.get("/model/{model_name}")
+@app.get("/model/{model_name}", status_code=status.HTTP_200_OK)
 def get_model(model_name: ModelName):
     if model_name == model_name.first:
         return {"Model Name": f"{model_name}"}
@@ -42,12 +50,12 @@ def get_model(model_name: ModelName):
         return {"Model Name": f"{model_name}"}
 
 
-@app.post("/createposts")
+@app.post("/createposts", status_code=status.HTTP_201_CREATED)
 def create_post(payload: dict = Body(...)):
     return {"message": f"title: {payload['title']} - comment: {payload['comment']}"}
 
 
-@app.get("/user/{user_id}/item/{item_id}")
+@app.get("/user/{user_id}/item/{item_id}", status_code=status.HTTP_200_OK)
 def read_user_item(user_id: int, item_id: int, q: Optional[str] = Query(None, max_length=50)):
     item = {"user": user_id, "item": item_id}
     if q:
@@ -55,7 +63,7 @@ def read_user_item(user_id: int, item_id: int, q: Optional[str] = Query(None, ma
     return item
     
 
-@app.get("/sample_post/{post}")
+@app.get("/sample_post/{post}", status_code=status.HTTP_200_OK)
 def sample_post(*, post: str = Path(..., title="The ID of post to get"), q: Optional[str] = None, state: boolean = False):
     post = {"Message": post}
     if state:
@@ -63,16 +71,21 @@ def sample_post(*, post: str = Path(..., title="The ID of post to get"), q: Opti
     return post
         
 
-@app.get("/user")
+@app.get("/user", status_code=status.HTTP_200_OK)
 def get_user(payload: User):
     return {"Payload": payload}
 
 
-@app.get("/getCookie/")
+@app.get("/getCookie/", status_code=status.HTTP_200_OK)
 async def getCookie(ads_id: Optional[str] = Cookie(None)):
     return {"ads_id": ads_id}
 
 
-@app.get("/getHeader/")
+@app.get("/getHeader/", status_code=status.HTTP_200_OK)
 async def getCookie(user_agent: Optional[str] = Header(None)):
     return {"user_agent": user_agent}
+
+
+@app.get("/getPayement/", response_model=Payment, status_code=status.HTTP_200_OK)
+def getPayement(pay: Payment):
+    return pay
